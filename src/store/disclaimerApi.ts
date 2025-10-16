8ddbafb8882c0e8593b0c70d857b7895ac3d6fcf
Nesat Nayem <nesatnayem042@gmail.com>
2025-10-16 09:03:50 +0600

    url update

--- src/store/disclaimerApi.ts
+++ src/store/disclaimerApi.ts
@@ -18,7 +18,7 @@ export const disclaimerApi = createApi({
   reducerPath: 'disclaimer',
   baseQuery: fetchBaseQuery({
     credentials: 'include',
-    baseUrl: 'https://api.atpuae.com/v1/api',
+    baseUrl: 'http://localhost:8080/v1/api',
     prepareHeaders: (headers, { getState }) => {
       const token = (getState() as IRootState).auth?.token
       if (token) {
