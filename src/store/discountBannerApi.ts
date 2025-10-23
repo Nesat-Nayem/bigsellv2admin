759b521e18b7590792acc72c973965556f1ea957
Nesat Nayem <nesatnayem042@gmail.com>
2025-10-23 11:18:31 +0600

    LOCAL URL

--- src/store/discountBannerApi.ts
+++ src/store/discountBannerApi.ts
@@ -19,7 +19,7 @@ export const discountBannerApi = createApi({
   reducerPath: 'discountBannerApi',
   baseQuery: fetchBaseQuery({
     credentials: 'include',
-    baseUrl: 'https://api.atpuae.com/v1/api',
+    baseUrl: 'http://localhost:8080/v1/api',
     prepareHeaders: (headers, { getState }) => {
       const token = (getState() as IRootState).auth?.token
       if (token) {
