a3b8866ab65f5b4ba0e20de4d51ac2e1c8fb971c
Nesat Nayem <nesatnayem042@gmail.com>
2025-10-29 13:20:00 +0600

    url update

--- src/store/aboutApi.ts
+++ src/store/aboutApi.ts
@@ -49,7 +49,7 @@ export const aboutApi = createApi({
   reducerPath: 'aboutApi',
   baseQuery: fetchBaseQuery({
     credentials: 'include',
-    baseUrl: 'http://localhost:8080/v1/api',
+    baseUrl: 'https://api.atpuae.com/v1/api',
     prepareHeaders: (headers, { getState }) => {
       const token = (getState() as IRootState).auth?.token
       if (token) {
