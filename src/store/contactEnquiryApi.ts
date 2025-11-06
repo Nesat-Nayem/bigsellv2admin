302235dfee72c5e9e22ce69fb20914ed609b9c0d
Nesat Nayem <nesatnayem042@gmail.com>
2025-11-06 11:26:58 +0600

    url update for deployment

--- src/store/contactEnquiryApi.ts
+++ src/store/contactEnquiryApi.ts
@@ -23,7 +23,7 @@ export const contactApi = createApi({
   reducerPath: 'contactApi',
   baseQuery: fetchBaseQuery({
     credentials: 'include',
-    baseUrl: 'http://localhost:8080/v1/api',
+    baseUrl: 'https://api.atpuae.com/v1/api',
     prepareHeaders: (headers, { getState }) => {
       const token = (getState() as IRootState).auth.token
       if (token) {
